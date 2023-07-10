1fa8160d118b10e09378913a99ece802651cf8b9
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-07-11 00:34:46 +0530

    Updated

--- booking.js
+++ booking.js
@@ -113,3 +113,20 @@ childrenDecBtn.addEventListener("click", () => {
     children.innerHTML = childrenNumber;
   }
 });
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
